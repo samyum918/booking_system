8d54975cb87f52c2e397f7a4e2545b991aad7995
Sam Yum <sam.yum@worldline.com>
2020-09-09 20:09:46 +0800

    modify store get all API to get by page

--- booking-backend/booking-core/src/main/java/com/flexible/booking/controller/StoreController.java
+++ booking-backend/booking-core/src/main/java/com/flexible/booking/controller/StoreController.java
@@ -1,11 +1,15 @@
 package com.flexible.booking.controller;
 
 import com.flexible.booking.dto.request.CreateStoreRequest;
+import com.flexible.booking.exception.ApiBadRequestException;
 import com.flexible.booking.exception.ApiResourceNotFoundException;
 import com.flexible.booking.model.Store;
 import com.flexible.booking.repository.StoreRepository;
 import com.flexible.booking.service.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -27,8 +31,15 @@ public class StoreController {
     }
 
     @GetMapping("/get")
-    public List<Store> getAll() {
-        return storeRepository.findAll();
+    public List<Store> getAll(@RequestParam(defaultValue = "1", required = false) Integer page,
+                              @RequestParam(defaultValue = "10", required = false) Integer limit) {
+        if(page < 1 || limit < 1) {
+            throw new ApiBadRequestException("Page and limit must be greater than or equal to 1");
+        }
+
+        Pageable pageable = PageRequest.of(page - 1, limit);
+        Page<Store> storesList = storeRepository.findAll(pageable);
+        return storesList.getContent();
     }
 
     @GetMapping("/get/{id:\\d+}")
